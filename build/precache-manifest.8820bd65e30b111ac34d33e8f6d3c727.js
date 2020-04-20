3278f8d2faae1e6c7cffe2e150af63c78c820a6f
MechaMahmud71 <50962084+MechaMahmud71@users.noreply.github.com>
2020-04-20 14:19:08 +0600

    Add files via upload

---
+++ build/precache-manifest.8820bd65e30b111ac34d33e8f6d3c727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983bbf35b7efd641220c5b3fdc044cd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09434cb0fcddf91e3458",
+    "url": "/static/css/main.a9dd824c.chunk.css"
+  },
+  {
+    "revision": "22abb81dc47fe6699618",
+    "url": "/static/js/2.3104b801.chunk.js"
+  },
+  {
+    "revision": "0b657c19c4cfb3d6362c7b59c64c0bc2",
+    "url": "/static/js/2.3104b801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09434cb0fcddf91e3458",
+    "url": "/static/js/main.4bac0b3c.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  }
+]);
